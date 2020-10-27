93517549f609533a310057edd7fea8d1be3678f9
drny85 <robertm3lendez@gmail.com>
2020-10-27 11:40:01 -0400

    fixed back res.sendfile

--- backend/server.js
+++ backend/server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
   app.get('*', (req, res) =>
-    req.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => {
